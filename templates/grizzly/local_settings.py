3de72d4fe2764a2113f24044415021d21bf4b8e1
James Page <james.page@canonical.com>
2013-07-16 09:22:30 +0100

    Fixup grizzly template

--- templates/grizzly/local_settings.py
+++ templates/grizzly/local_settings.py
@@ -116,7 +116,7 @@ COMPRESS_OFFLINE = {{ compress_offline }}
 # ]
 
 OPENSTACK_HOST = "127.0.0.1"
-OPENSTACK_KEYSTONE_URL = "http://{{ service_host }}:{{ service_port }]/v2.0"
+OPENSTACK_KEYSTONE_URL = "http://{{ service_host }}:{{ service_port }}/v2.0"
 OPENSTACK_KEYSTONE_DEFAULT_ROLE = "{{ default_role }}"
 
 # Disable SSL certificate checks (useful for self-signed certificates):
